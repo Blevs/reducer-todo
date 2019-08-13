1abd9db771b5e538bac9a0443c003b7beffde90e
Henry Blevins <heblevi@gmail.com>
2019-08-13 13:26:50 -0400

    Completed solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
+import {initialState, todoReducer} from './reducers/todoReducer.js';
+import TodoList from './components/TodoList.js';
+import TodoForm from './components/TodoForm.js';
 
 function App() {
+  const [state, dispatch] = useReducer(todoReducer, initialState);
+  const toggle = id => dispatch({ type: "TOGGLE_TODO", payload: id });
+  const addTodo = item => dispatch({ type: "ADD_TODO", payload: item });
+  const clearCompleted = () => dispatch({ type: "CLEAR_COMPLETED" });
   return (
-    <div className="App">
+    <div className="todo">
+      <h2>Todos</h2>
+      <div className="todo-body">
+        <TodoForm addTodo={addTodo} clearCompleted={clearCompleted} />
+        <TodoList todos={state.todos} toggle={toggle} />
+      </div>
     </div>
   );
 }
