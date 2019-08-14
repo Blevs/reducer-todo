6b455921ff8350c2121607d7456259a454e2687b
Henry Blevins <heblevi@gmail.com>
2019-08-14 15:26:44 -0400

    Complete recitation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { initialState, reducer } from './reducer';
 import './App.css';
 
+import TodoList from './components/TodoList.js';
+import TodoForm from './components/TodoForm.js';
+
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log('state', state);
+
+  const addTodo = item => {
+    dispatch({ type: "ADD_TODO", payload: item });
+  };
+
+  const toggleTodo = id => {
+    dispatch({ type: "TOGGLE_TODO", payload: id });
+  };
+
+  const clearCompleted = () => {
+    dispatch({ type: "CLEAR_COMPLETED" });
+  };
+
   return (
     <div className="App">
+      <TodoList todoArray={state.todoArray} toggleTodo={toggleTodo} />
+      <TodoForm addTodo={addTodo} clearCompleted={clearCompleted} />
     </div>
   );
 }
